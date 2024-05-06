3397670ce2c022a8484b3212e2d3c6f53267f559
Luciano Chagnaud <lucianochagnaud@gmail.com>
2024-05-06 09:26:32 -0300

    modificacion solicitud post empleado

--- buen_sabor_dashboard/src/components/Empleado/Empleado.tsx
+++ buen_sabor_dashboard/src/components/Empleado/Empleado.tsx
@@ -24,7 +24,7 @@ const EmpleadoComponent = () => {
 
   const fetchEmpleados = async () => {
     try {
-      const empleados = await empleadoService.getAll(url + 'empleados');
+      const empleados = await empleadoService.getAll(url + 'empleado');
       dispatch(setEmpleado(empleados)); 
       setFilteredData(empleados); 
     } catch (error) {
